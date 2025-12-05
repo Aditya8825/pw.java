729081679b314d453113b38784a0256d27e8c4ad
Aditya8825 <aditya998887766@gmail.com>
2025-12-06 00:54:29 +0530

    2DArray
    2D array ,qs

---
+++ main.java
@@ -0,0 +1,65 @@
+// import java.util.*;
+// public class main {
+//     public static void main(String[] args) {
+       // Scanner sc=new Scanner(System.in);
+   
+
+        // print a 2d array take input from user ,,,,,,,,,,,,,,,,,,,,,
+//       System.out.println("eneter the number of rows:");
+//       int rows=sc.nextInt();
+ 
+
+//       System.out.println("enter the number of columns:");
+//       int columns=sc.nextInt();
+
+//       int [][]arr=new int[rows][columns];
+
+//       System.out.println("enter the elemnt in row:");
+//          for(int i=0; i<rows;  i++){
+//             for(int j=0; j<columns; j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+// System.out.println("enter the elemnt in column:");
+// for(int i=0; i<rows; i++){
+//     for(int j=0; j<columns; j++ ){
+//       System.out.print(arr[i][j]+" ");
+// }
+// System.out.println();
+// }
+// }
+// }
+
+//tranpose a  matrix
+// Scanner sc=new Scanner(System.in);
+
+// System.out.println("enter number of rows:");
+// int rows=sc.nextInt();
+
+// System.out.println("enter number of columns");
+// int columns=sc.nextInt();
+//   int [][]arr=new int[rows][columns];
+
+//   System.out.println("enter the matrix elements");
+//    for(int i=0; i<rows; i++){
+//     for(int j=0; j<columns; j++){
+//         arr[i][j]=sc.nextInt(); 
+//     }
+//    }
+//    //create transpose
+//    int [][]trans=new int[columns][rows];
+
+//    for(int i=0; i<rows; i++){
+//     for(int j=0; j<columns; j++){
+//         trans[j][i]=arr[i][j];     
+//     }
+//    }
+//  System.out.println("transpose of the matrix:");
+//    for(int i=0; i<rows; i++){
+//     for(int j=0; j<columns; j++){
+//         System.out.print(trans[i][j]+" ");     
+//     }
+//    System.out.println();
+//    }
+// }}
+
