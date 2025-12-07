116e15d1ed62c3133fffe5aae31fcd12c67530a4
Aditya8825 <aditya998887766@gmail.com>
2025-12-07 15:46:14 +0530

    java string
    string problem

---
+++ string.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+// public class string {
+//     public static void main(String[] args) {
+// print a string in revserse order<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,         
+// String str="hello ";
+// String  reversed ="";
+//  for(int i=str.length()-1; i>0; i--){
+//     reversed+=str.charAt(i);
+//  }
+//   System.out.println("original string:" +str);
+//   System.out.println("reversed  string:"+reversed);
+// }}
+
+// same question taking input from user <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// public class string {
+//     public static void main(String[] args) {
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter a string:");
+// String input=sc.nextLine();
+
+// String reversed="";
+// for( int i=input.length()-1; i>=0; i--){
+//      reversed+=input.charAt(i);    
+// }
+//  System.out.println("original string:"+reversed);
+//      sc.close();
+// }}
+ 
+//Check for palindrome<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+// System.out.println("enter a string:");
+// String input=sc.nextLine();
+// String reversed="";
+// for(int i=input.length()-1;  i>=0; i--){
+//     reversed+=input.charAt(i);   
+// }
+// if(input.equals(reversed)){
+//     System.out.println(" it is palindrome:");
+// }else{
+//     System.out.println("it is not a palindrome:");
+// }
+// }
+// }
